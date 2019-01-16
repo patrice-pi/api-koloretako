ea3fe54e1407e5add12811626f16393ffaae0159
patrice-pi <patrice.picollet@hotmail.fr>
2019-01-16 13:29:36 +0000

    animation game

---
+++ public/js/game.js
@@ -0,0 +1,6 @@
+jQuery(function($){
+    setTimeout(function(){
+        $('#game').css({'opacity':'0'});
+        $('#game').animate({'opacity':'1'});
+    })
+});
--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -41,13 +41,15 @@
 
     <h1 class="text-center text-uppercase my-5">Le jeu</h1>
 
-    <div class="" id="game">
+    <div class="" id="game"></div>
 
-    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="{{ asset("/js/game.js") }}">
+
+    </script>
 </body>
 
 </html>
